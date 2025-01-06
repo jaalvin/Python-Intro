3034220b54dbb0883522e988d48b7f5c217a32d3
Alvin Acquaah Julian Turkson <158464222+jaalvin@users.noreply.github.com>
2025-01-06 19:28:46 +0000

    A Simple python script that prints Hello, World and the results of basic arithmetic

---
+++ month1/day1.py
@@ -0,0 +1,8 @@
+'''A Simple python script that prints "Hello, World!" and the results of basic arithmetic'''
+
+#Prints "Hello, World!"
+print("Hello, World!")
+
+#The results of a basic arithmetic
+diff = 30-6
+print("The difference between 30 and 6 is ", diff)
