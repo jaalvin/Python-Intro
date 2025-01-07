a2a903786c04accd9e7f3a524814c5d9c3a245b1
Alvin Acquaah Julian Turkson <158464222+jaalvin@users.noreply.github.com>
2025-01-07 16:21:22 +0000

    Update day2.py

--- month1/day2.py
+++ month1/day2.py
@@ -1,14 +1,17 @@
-'''Create a program that takes user input for their name and age, then prints a greeting with their name and calculates the year they were born.'''
+'''A simple program to greet the user and output the user's birth year'''
+
+from datetime import date 
 
 #Takes in user's name and age as input
 Name = input("What is your name? ")
-Age = int(input("What is your age? "))
+Age = int(input("What is your age? "))  
 
 #Prints out a greeting with their name
-print("Hello, ", Name)
+print("Hola,",Name)
 
 #Calculates the user's year of birth from their age
-Year = 2024 - Age
+Current_year = date.today().year
+Birth_year = Current_year - Age
 
 #Prints their year of birth
-print("You were born in ",Year)
+print("You were born in",Birth_year)
