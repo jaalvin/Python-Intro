656dff77b85d0b5ce875b88af7340264fa089218
Alvin Acquaah Julian Turkson <158464222+jaalvin@users.noreply.github.com>
2025-01-08 21:14:42 +0000

    Add files via upload

---
+++ month1/day3.py
@@ -0,0 +1,19 @@
+'''Build a simple age checker: Ask the user for their age and tell them if they are eligible for certain 
+services (e.g., "You are eligible to vote" or "You are too young to vote").'''
+
+
+#Brief the user on the program
+print("This is the enlisting point for the upcoming Fourth Great Ninja War.")
+
+#Ask the user for their age
+age = int(input("How old are you? \n"))
+
+#Check if user's age is eligible
+if age>=60:
+    print("Congrats Soldier!, thank you for your service. \nYou can retire now.")
+
+elif 18<age<60:
+    print("You are going nowhere lazy comrade! \nThere's a war to win.")
+
+else:
+    print("Relax Comrade! \nChop rice, chop rice.")
