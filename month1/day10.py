5acff3c04f03296bdb5c3ba08b1e358035b7cb80
Alvin Acquaah Julian Turkson <158464222+jaalvin@users.noreply.github.com>
2025-01-15 14:59:21 +0000

    Introduction to Libraries

---
+++ month1/day10.py
@@ -0,0 +1,7 @@
+#A Python script that calculates the square root of a number using the math library.
+
+import math
+
+num = float(input("Enter a number to find the square root: "))
+result = math.sqrt(num)
+print(f"The square root is: {result}")
