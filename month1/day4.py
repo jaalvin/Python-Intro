8b2784199682cf6a2dc1e1ef24503d92fe5d7808
Alvin Acquaah Julian Turkson <158464222+jaalvin@users.noreply.github.com>
2025-01-15 14:00:32 +0000

    Loops

--- month1/day4.py
+++ month1/day4.py
@@ -1,4 +1,4 @@
-#A simple "countdown" program that counts down from 10 to 1 using a while loop.
+#A simple "countdown" program that counts down from 10 to 1 using a while loop. 
 
 i = 10
 while i > 0:
