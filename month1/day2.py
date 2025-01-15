46aac6137e07bca5750275eb59ff64fbd0f0c283
Alvin Acquaah Julian Turkson <158464222+jaalvin@users.noreply.github.com>
2025-01-15 13:50:57 +0000

    Variables and Data Types

--- month1/day2.py
+++ month1/day2.py
@@ -1,6 +1,6 @@
 '''A simple program to greet the user and output the user's birth year'''
 
-from datetime import date 
+from datetime import date
 
 #Takes in user's name and age as input
 Name = input("What is your name? ")
